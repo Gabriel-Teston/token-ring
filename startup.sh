a4bbf4badeb810f95ce09cc58440862168367ea8
Gabriel-Teston <gtestonvasconcelos@gmail.com>
2022-12-04 20:20:26 -0300

    git repo url

--- startup.sh
+++ startup.sh
@@ -4,11 +4,10 @@ apt-get install -yq git supervisor python3-pip python3-venv jq
 
 # Fetch source code
 mkdir /opt/app
-git clone https://github.com/Gabriel-Teston/clock-sync.git /opt/app
+git clone https://github.com/Gabriel-Teston/token-ring.git /opt/app
 
 # Python environment setup
 python3 -m venv /opt/app/env
-source /opt/app/env/bin/activate# sudo curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/requirements" -H "Metadata-Flavor: Google" | sudo tee /opt/app/requirements.txt
 pip3 install -r /opt/app/requirements.txt
 
 export INSTANCE_A_ADRESS=$(sudo curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/instance_a_address" -H "Metadata-Flavor: Google")
